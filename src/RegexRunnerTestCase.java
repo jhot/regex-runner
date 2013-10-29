6d614a3903ea01e5e21f48169426442bf1a88e48
John Hotovy <john.hotovy@gmail.com>
2013-10-28 21:57:48 -0500

    Add several more test cases

--- src/RegexRunnerTestCase.java
+++ src/RegexRunnerTestCase.java
@@ -128,6 +128,223 @@ public class RegexRunnerTestCase
 		assertFalse(acceptsString("aaa*b*a*a", "bbaa"));
 	}
 	
+	// any string with an even length
+	@Test
+	public void extraTestCase1_1()
+	{
+		assertTrue(acceptsString("((a|b)(a|b))*", "e"));
+	}
+	
+	@Test
+	public void extraTestCase1_2()
+	{
+		assertFalse(acceptsString("((a|b)(a|b))*", "b"));
+	}
+	
+	@Test
+	public void extraTestCase1_3()
+	{
+		assertTrue(acceptsString("((a|b)(a|b))*", "ba"));
+	}
+	
+	@Test
+	public void extraTestCase1_4()
+	{
+		assertFalse(acceptsString("((a|b)(a|b))*", "aaa"));
+	}
+	
+	@Test
+	public void extraTestCase1_5()
+	{
+		assertTrue(acceptsString("((a|b)(a|b))*", "bbab"));
+	}
+	
+	// any string with an odd length
+	@Test
+	public void extraTestCase2_1()
+	{
+		assertFalse(acceptsString("((a|b)(a|b))*(a|b)", "e"));
+	}
+	
+	@Test
+	public void extraTestCase2_2()
+	{
+		assertTrue(acceptsString("((a|b)(a|b))*(a|b)", "b"));
+	}
+	
+	@Test
+	public void extraTestCase2_3()
+	{
+		assertFalse(acceptsString("((a|b)(a|b))*(a|b)", "ba"));
+	}
+	
+	@Test
+	public void extraTestCase2_4()
+	{
+		assertTrue(acceptsString("((a|b)(a|b))*(a|b)", "aaa"));
+	}
+	
+	@Test
+	public void extraTestCase2_5()
+	{
+		assertFalse(acceptsString("((a|b)(a|b))*(a|b)", "bbab"));
+	}
+	
+	// any string with substring aba
+	@Test
+	public void extraTestCase3_1()
+	{
+		assertFalse(acceptsString("(a|b)*aba(a|b)*", "ab"));
+	}
+	
+	@Test
+	public void extraTestCase3_2()
+	{
+		assertFalse(acceptsString("(a|b)*aba(a|b)*", "ba"));
+	}
+	
+	@Test
+	public void extraTestCase3_3()
+	{
+		assertFalse(acceptsString("(a|b)*aba(a|b)*", "bba"));
+	}
+	
+	@Test
+	public void extraTestCase3_4()
+	{
+		assertTrue(acceptsString("(a|b)*aba(a|b)*", "aba"));
+	}
+	
+	@Test
+	public void extraTestCase3_5()
+	{
+		assertFalse(acceptsString("(a|b)*aba(a|b)*", "aaaaaaaab"));
+	}
+	
+	@Test
+	public void extraTestCase3_6()
+	{
+		assertTrue(acceptsString("(a|b)*aba(a|b)*", "aaaaaaaaba"));
+	}
+	
+	// any string without substring ba
+	@Test
+	public void extraTestCase4_1()
+	{
+		assertTrue(acceptsString("a*b*", "e"));
+	}
+	
+	@Test
+	public void extraTestCase4_2()
+	{
+		assertFalse(acceptsString("a*b*", "ba"));
+	}
+	
+	@Test
+	public void extraTestCase4_3()
+	{
+		assertTrue(acceptsString("a*b*", "aaaabbbb"));
+	}
+	
+	@Test
+	public void extraTestCase4_4()
+	{
+		assertTrue(acceptsString("a*b*", "aaaab"));
+	}
+	
+	@Test
+	public void extraTestCase4_5()
+	{
+		assertFalse(acceptsString("a*b*", "bbbbbbba"));
+	}
+	
+	// any string with an even number of b's
+	@Test
+	public void extraTestCase5_1()
+	{
+		assertTrue(acceptsString("a*(ba*ba*)*", "bb"));
+	}
+	
+	@Test
+	public void extraTestCase5_2()
+	{
+		assertFalse(acceptsString("a*(ba*ba*)*", "bbb"));
+	}
+	
+	@Test
+	public void extraTestCase5_3()
+	{
+		assertTrue(acceptsString("a*(ba*ba*)*", "bbabb"));
+	}
+	
+	@Test
+	public void extraTestCase5_4()
+	{
+		assertFalse(acceptsString("a*(ba*ba*)*", "bbab"));
+	}
+	
+	@Test
+	public void extraTestCase5_5()
+	{
+		assertTrue(acceptsString("a*(ba*ba*)*", "aaaaabaaabaaaabbaaabbaabab"));
+	}
+	
+	// any string without substring bb
+	@Test
+	public void extraTestCase6_1()
+	{
+		assertTrue(acceptsString("(a|ba)*(e|b)", "e"));
+	}
+	
+	@Test
+	public void extraTestCase6_2()
+	{
+		assertTrue(acceptsString("(a|ba)*(e|b)", "aaaba"));
+	}
+	
+	@Test
+	public void extraTestCase6_3()
+	{
+		assertFalse(acceptsString("(a|ba)*(e|b)", "aaabba"));
+	}
+	
+	@Test
+	public void extraTestCase6_4()
+	{
+		assertTrue(acceptsString("(a|ba)*(e|b)", "aabaab"));
+	}
+	
+	@Test
+	public void extraTestCase6_5()
+	{
+		assertFalse(acceptsString("(a|ba)*(e|b)", "aaabaabaabbaab"));
+	}
+	
+	// difficult to parse string
+	@Test
+	public void extraTestCase7_1()
+	{
+		assertTrue(acceptsString("(bb|bbbb|bbbbbb)*(((((a)))))*|(e)", "e"));
+	}
+	
+	@Test
+	public void extraTestCase7_2()
+	{
+		assertFalse(acceptsString("(bb|bbbb|bbbbbb)*(((((a)))))*|(e)", "ba"));
+	}
+	
+	@Test
+	public void extraTestCase7_3()
+	{
+		assertFalse(acceptsString("(bb|bbbb|bbbbbb)*(((((a)))))*|(e)", "bbb"));
+	}
+	
+	@Test
+	public void extraTestCase7_4()
+	{
+		assertTrue(acceptsString("(bb|bbbb|bbbbbb)*(((((a)))))*|(e)", "bbbbbbbba"));
+	}
+	
 	private boolean acceptsString(String regex, String inputString)
 	{
 		NFA nfa = NFAParser.parseStringToNfa(regex);
